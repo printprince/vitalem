74661a3fab107b05db23731a0a4ab92e83fbc47f
printprince <kairollaadilet@gmail.com>
2025-06-05 09:28:19 +0500

    appointment_service

--- appointment_service/internal/models/schedule.go
+++ appointment_service/internal/models/schedule.go
@@ -14,7 +14,7 @@ type DoctorSchedule struct {
 	Name     string    `gorm:"type:varchar(255);not null" json:"name"` // "Основное расписание"
 
 	// Дни недели: [1,2,3,4,5] = Пн-Пт
-	WorkDays []int `gorm:"type:jsonb" json:"work_days"`
+	WorkDays []int `gorm:"type:integer[]" json:"work_days"`
 
 	// Время работы
 	StartTime string `gorm:"type:varchar(5);not null" json:"start_time"` // "09:00"
